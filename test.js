931a8504a991464755efdf7a0a9cf56c8ad9dfe8
hollyhudson <hollyf@swcp.com>
2017-05-28 14:05:51 -0400

    made a pulsing square

--- test.js
+++ test.js
@@ -1,14 +1,33 @@
 
 function setup() {
-	createCanvas(1000,600);
+	createCanvas(800,500);
+	xcenter = width/2;
+	ycenter = height/2;
+
+	x = xcenter - 200;
+	y = ycenter - 200;
+
+	dir = 1;
+	size = 400;
+
+	noFill();
+	frameRate(20);
 }
 
 function draw() {
-	//rect(10,10,20,20);
-	for(x = 10; x < 980; x += 20){
-		for(y = 10; y < 580; y += 20){
-			rect(x, y, 10, 10);
-		}
+	background(255, 200, 250, 50);
+	rect(x, y, size, size);
+
+	//  set up next box
+
+	x = x + (dir*30);
+	y = y + (dir*30);
+	size =  size + ((-1)*dir*60)
+	
+	if (x > xcenter - 30) {
+		dir = -1;
+	} else if (x < xcenter - 200) {
+		dir = 1;	
 	}
 
 }
