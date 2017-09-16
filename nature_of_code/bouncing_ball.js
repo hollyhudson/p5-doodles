3f964cbf7f0a296b83afd9d58c5ce49762d4b1ff
Holly <hollyf@swcp.com>
2017-09-16 14:43:14 -0400

    committing for remote work

---
+++ nature_of_code/bouncing_ball.js
@@ -0,0 +1,65 @@
+/* Bouncing Ball, from http://natureofcode.com/book/introduction/ */
+
+function setup() {
+	createCanvas(1000,600);
+	background(80, 80, 80);
+	var ball_center = createVector(width/2, height/2);
+}
+
+function draw() {
+	background(80, 80, 80);
+	for 
+	
+}
+
+// class definition
+function Ball() {
+	circle
+	
+	// class methods: display() and step()
+	// show the walker
+	this.display = function() {
+		stroke(200,150,250);
+		point(this.x,this.y);
+	}
+	// have the walker take a step
+	this.step = function() {
+
+		/*
+		// pick from 9 possible next postitions instead of just 4
+		// var stepx = random() * (max - min) + min
+		var stepx = (random() * (1 - (-1))) + (-1);
+		var stepy = (random() * (1 - (-1))) + (-1);
+
+		this.x += stepx;
+		this.y += stepy;
+		*/
+
+		// pick a random direction
+		var choice = random(1);
+		if (choice < 0.25) {
+			this.x = this.x + 3;
+		} else if (choice < 0.5 ) {
+			this.x = this.x - 3;
+		} else if (choice < 0.75) {
+			this.y = this.y + 3;
+		} else {
+			// this.y--; will give a line instead of stipple.
+			this.y = this.y - 3;
+		}
+	}
+}
+
+var w;
+
+function setup() {
+	createCanvas(1000,600);
+	w = new Walker();
+	background(80, 80, 80);
+	
+}
+
+function draw() {
+	w.step();
+	w.display();	
+}
