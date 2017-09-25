2530ef28b80d589f78f5364d4ede6f8edda17e2b
hollyhudson <hollyf@swcp.com>
2017-09-25 13:25:29 -0400

    starting collisions lesson #24

---
+++ webdev/object_collision.js
@@ -0,0 +1,47 @@
+// Checking for intersection of objects for collision
+
+var bubble_num = 10;
+var bubbles = [];
+
+function setup() {
+	createCanvas(800, 500);
+	for (var i = 0; i < bubble_num; i++) {
+		// the keyword "new" means use the constructor
+		bubbles[i] = new Bubble(random(width), random(height), 24);
+	}
+}
+
+function draw() {
+	background(80);
+	if (bubbles.length > 50) {
+		var extras = bubbles.length - 50;
+		for (var i = 0; i < extras; i++) {
+			bubbles.splice(0,1);
+		}
+	}
+
+	// You need to loop backwards when removing items from an array
+	// or else you'll end up skipping the item after the one you deleted
+	for (var i = bubbles.length - 1; i >= 0 ; i--) {
+		bubbles[i].update();
+		bubbles[i].display();
+		/*
+		if (bubbles[i].r <= 0) {
+			bubbles.splice(i,1);
+		}
+		*/
+		if (bubbles[i].isFinished()) {
+			bubbles.splice(i, 1);	
+		}
+	}
+}
+
+function mousePressed() {
+	for (var i = 0; i < bubbles.length; i++) {
+		bubbles[i].clicked();
+	}
+}
+
+function mouseDragged() {
+	bubbles.push(new Bubble(mouseX, mouseY, 24));
+}
