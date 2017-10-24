ddf1fb5dac0e1d1c27e95f7c1670eaa0029dd85a
Holly <hollyf@swcp.com>
2017-10-24 16:02:28 -0400

    fixed the problem with the input function and have a working font size slider #32

--- webdev/html_fontslider.js
+++ webdev/html_fontslider.js
@@ -1,5 +1,6 @@
 var paragraph;
 var sizeSlider;
+var sizeReport;
 
 function setup() {
 	paragraph = createP("The quick brown fox jumped over the lazy dog.  Four score and 7 years ago.  Tomorrow, tomorrow and tomorrow, creeps in it's petty pace from day to day, to the last syllable of recorded time, and all our yesterdays have lighted fools the way to dusty death.  Out, out brief candle, life is but a walking shadow.  A poor player who struts and frets his hour upon the stage and is heard of no more.  It is a tale told by an idiot, full of sound and fury, signifying nothing.");	
@@ -7,10 +8,13 @@ function setup() {
 	sizeSlider = createSlider(10, 64, 16);
 	sizeSlider.input(updateSize);
 
+	sizeReport = createSpan("font size: " + sizeSlider.value() + " pt");
+
 }
 
 function updateSize() {
 	paragraph.style("font-size", sizeSlider.value() + "pt");
+	sizeReport.html("font size: " + sizeSlider.value() + " pt");
 }
 
 function draw() {
