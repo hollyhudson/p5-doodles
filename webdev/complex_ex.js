c9f534a2491ff0a99eb316419e2c96482c779cbe
Holly <hollyf@swcp.com>
2017-10-24 19:07:29 -0400

    started making a more complex page to practice some css manipulation with p5.js #34

---
+++ webdev/complex_ex.js
@@ -0,0 +1,13 @@
+// Here's a more complex example to play with css with p5.js
+
+var canvas;
+
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0,0); 			// put the canvas at the top
+	canvas.style('z-index', '-1'); 	// put it behind the page content
+}
+
+function draw() {
+	background(100);
+}
