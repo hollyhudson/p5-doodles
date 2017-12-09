a440c5a6e9f26e028adb10b1a6561c647b10caf4
Holly <hollyf@swcp.com>
2017-12-08 19:49:50 -0500

    port of snowflake code from processing to p5js is working

--- data/Astronaut.js
+++ data/Astronaut.js
@@ -5,30 +5,29 @@ function Astronaut(posX, posY, name, ship) {
 	this.name = name;
 	this.ship = ship;
 	//this.astro_image = astro_image;
-	this.x_dir = random(-1,1);
-	this.y_dir = random(-1,1);
+	let x_dir = Math.floor((Math.random() * 1) -1);
+	let y_dir = Math.floor((Math.random() * 1) -1);
 
 	this.display = function() {
 		//color(0);
 		//text(name, this.x, this.y+10);
 		//text(ship, this.x, this.y+20);
-		//scale(.1);
-		//image(astro_image, this.x, this.y)
+		scale(.1);
+		image(astro_image, this.x, this.y)
 	}
 
 	this.update = function() {
-		console.log("in Astronaut.update");
-	/*
+		console.log(x_dir);
 		// float randomly
-		if ((parseInt(random(10)) == 0) {
-			this.x_dir *= -1; // change direction
+		if (parseInt(random(30)) == 0) {
+			x_dir *= Math.floor((Math.random() * 1) -1); // change direction
 		}
-		if ((parseInt(random(10)) == 0) {
-			this.y_dir *= -1; // change direction
-		}
-		this.x += this.x_dir;
-		this.y += this.y_dir;
-	*/
 
+		if (parseInt(random(30)) == 0) {
+			y_dir *= Math.floor((Math.random() * 1) -1); // change direction
+		}
+
+		this.x += x_dir;
+		this.y += y_dir;
 	}
 }
--- data/json_url.js
+++ data/json_url.js
@@ -15,22 +15,38 @@ function gotdata(data) {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+
+/*
 	if (astro_data) {
 		for (let i = 0; i < astro_data.number; i++) {
 			crew.push(new Astronaut(
 				random(20, windowWidth - 20),
 				random(20, windowHeight - 20),
-				astro_data.name,
-				astro_data.craft));
-				//astro_image));	
+				astro_data.people[i].name,
+				astro_data.people[i].craft));
 		}
 	}
+*/
 }
 
 function draw() {
-	background(255);
+	background(0);
+	noStroke();
+
+	fill('rgba(255, 100, 255, 0.5)');
+	rect(50, 50, 150, 100);
+
+	translate(50,50);
+	fill('rgba(100, 255, 255, 0.5)');
+	rect(0, 0, 100, 150);
+
+	translate(mouseX, mouseY);
+	fill('rgba(200, 255, 255, 0.5)');
+	ellipse(0,0, 200);
+/*
 	for (let i = 0; i < crew.length; i++) {	
 		crew[i].update();
 		crew[i].display();
 	}
+*/
 }
